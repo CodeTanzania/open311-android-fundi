2007360fd916b4f78d981146b106d624b0f4366e
Benson Maruchu <benmaruchu@gmail.com>
2017-12-03 20:23:33 +0300

    initialize repository

---
+++ app/src/main/java/com/github/codetanzania/open311/android/fundi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.codetanzania.open311.android.fundi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
