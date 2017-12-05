e1d8a63dff05759fc1bfe23d453ec888fda516e0
Benson Maruchu <benmaruchu@gmail.com>
2017-12-05 11:14:57 +0300

    add roboelectric as testing framework into the project

--- app/src/test/java/com/github/codetanzania/open311/android/fundi/ExampleUnitTest.java
+++ app/src/test/java/com/github/codetanzania/open311/android/fundi/ExampleUnitTest.java
@@ -1,14 +1,18 @@
 package com.github.codetanzania.open311.android.fundi;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
